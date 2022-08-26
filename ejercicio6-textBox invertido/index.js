9409970670e8cfedaaf2a04519d2e2e13bb43a74
Arturo Blanco <blancoarturo92@gmail.com>
2022-08-25 22:16:13 -0300

    se modifica ejercicio 6 para limpiar campo texto invertido

--- ejercicio6-textBox invertido/index.js
+++ ejercicio6-textBox invertido/index.js
@@ -1,21 +1,18 @@
 "use strict";
 let inputText= document.querySelector('.text');
-let invertedText='';
 
 let btnStart=document.querySelector('.btnInvert');
 btnStart.addEventListener('click', () => {
 let texto= inputText.value;
-//document.querySelector('.textEnd').innerHTML= "";
 document.querySelector('.textStart').innerHTML= texto;
-//document.querySelector('.textEnd').innerHTML= "";
 inverText(texto);
-}
-)
+})
 
 function inverText(texto) {
+    let invertedText="";
     for(let i = texto.length-1; i>=0 ; i--) {
         invertedText += texto[i]
     }
-   //document.querySelector('.textEnd').innerHTML= "";
+    document.querySelector('.textEnd').innerHTML= "";
     document.querySelector('.textEnd').innerHTML= invertedText;
 }
